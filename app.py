aa08a010152d3f93f19a5d8275bea968d6b6e9c9
Saikrish786 <g15031975@gmail.com>
2026-05-02 08:57:38 +0530

    fix app formatting

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
+import sqlite3
 
 app = Flask(__name__)
+app.secret_key = "secret123"
 
 @app.route("/")
 def home():
+    return redirect("/login")
+
+@app.route("/login")
+def login():
     return render_template("login.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=10000)
