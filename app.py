136afc4f97232928a2fb0a211c8e3202c05f2809
Saikrish786 <g15031975@gmail.com>
2026-05-02 14:29:58 +0530

    fix app.py final

--- app.py
+++ app.py
@@ -5,12 +5,10 @@ import os
 app = Flask(__name__)
 app.secret_key = "secret123"
 
-# HOME
 @app.route("/")
 def home():
     return redirect("/login")
 
-# REGISTER
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -20,7 +18,6 @@ def register():
 
         conn = sqlite3.connect("students.db")
         cursor = conn.cursor()
-
         cursor.execute("INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
                        (name, email, password))
         conn.commit()
@@ -30,7 +27,6 @@ def register():
 
     return render_template("register.html")
 
-# LOGIN
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -39,7 +35,6 @@ def login():
 
         conn = sqlite3.connect("students.db")
         cursor = conn.cursor()
-
         cursor.execute("SELECT * FROM users WHERE email=? AND password=?",
                        (email, password))
         user = cursor.fetchone()
@@ -51,36 +46,30 @@ def login():
 
     return render_template("login.html")
 
-# DASHBOARD
 @app.route("/dashboard")
 def dashboard():
     conn = sqlite3.connect("students.db")
     cursor = conn.cursor()
-
     cursor.execute("SELECT * FROM students")
     students = cursor.fetchall()
     conn.close()
 
     return render_template("dashboard.html", students=students)
 
-# DELETE
 @app.route("/delete/<int:id>")
 def delete(id):
     conn = sqlite3.connect("students.db")
     cursor = conn.cursor()
-
     cursor.execute("DELETE FROM students WHERE id=?", (id,))
     conn.commit()
     conn.close()
 
     return redirect("/dashboard")
 
-# LOGOUT
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect("/login")
 
-# RUN
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
